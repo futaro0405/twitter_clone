6d30af7e07f574acbc6daf974712d90d8ed9d535
futaro0405 <fkswtkm0405@gmail.com>
2024-03-19 22:25:50 +0900

    feat: update login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
   with_options presence: true do
     validates :telephone
     validates :birth_date
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_12_095119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
