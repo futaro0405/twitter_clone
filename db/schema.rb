d1e258240513656595da0e4fb3c16b2f027fdaea
futaro0405 <fkswtkm0405@gmail.com>
2024-04-03 18:08:07 +0900

    feat: migration post

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_25_040518) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_125616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,7 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_040518) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
-    t.string "name", null: false
     t.string "telephone", null: false
     t.datetime "birth_date", null: false
     t.datetime "created_at", null: false
@@ -46,4 +61,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_040518) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
