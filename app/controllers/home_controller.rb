08b8ec1361e0e9b603afc3d2183258e74e0c383e
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-04-15 23:44:30 +0900

    fixed relationships models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,13 @@
 
 class HomeController < ApplicationController
   def index
-    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(10)
-    @post_follow = Post.current_user.followings.order("created_at DESC").page(params[:page]).per(10)
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page_all]).per(10)
+    if current_user.followings.present?
+      @followers = current_user
+    else
+      @followers = current_user.followings
+    end
+    @post_follow = Post.where(user_id: @followers).order("created_at DESC").page(params[:page_follow]).per(10)
     @post = Post.new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,14 @@ class User < ApplicationRecord
   has_one_attached :image_avatar
   has_one_attached :image_cover
   has_many :posts, dependent: :destroy
+
+  # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :follow
 
   def self.create_unique_string
     SecureRandom.uuid
