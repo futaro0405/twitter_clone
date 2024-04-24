2112684c7580b0332b4dfcb472fc15a79aeab68d
futaro0405 <fkswtkm0405@gmail.com>
2024-04-24 18:11:09 +0900

    feat: fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 7.times do |n|
-  User.find_or_create_by!(id: n) do |user|
-    user.name = "username0#{n}"
+  User.find_or_create_by!(name: "username0#{n}") do |user|
     user.email = "username0#{n}@example.com"
     user.password = 'password'
     user.telephone = "0801234567#{n}"
@@ -29,10 +28,25 @@
   end
 end
 
+# ActiveRecord::Base.connection.tables.each do |t|
+#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
+# end
+
+User.all.each do |user|
+  3.times do |m|
+    Post.create!(
+      user_id: user.id,
+      content: "No. #{m} test content"
+      images: ActiveStorage::Blob.create_and_upload!(
+      io: File.open(Rails.root.join('app/assets/images/dummy.jpg').to_s), filename: 'dummy.jpg'
+    )
+    )
+  end
+end
+
 6.times do |m|
   Post.find_or_create_by!(user_id: m) do |post|
-    post.content = "user_id: #{m} test_content test_content test_content test_content test_content"
-
+    post.content = "test_content test_content test_content test_content test_content"
     post.images = ActiveStorage::Blob.create_and_upload!(
       io: File.open(Rails.root.join('app/assets/images/dummy.jpg').to_s), filename: 'dummy.jpg'
     )
@@ -41,4 +55,20 @@ end
 
 Relationship.find_or_create_by(follow_id: 1) do |user|
   user.followed_id = 2
+end
+
+Relationship.find_or_create_by(follow_id: 2) do |user|
+  user.followed_id = 3
+end
+
+Relationship.find_or_create_by(follow_id: 3) do |user|
+  user.followed_id = 4
+end
+
+Relationship.find_or_create_by(follow_id: 4) do |user|
+  user.followed_id = 5
+end
+
+Relationship.find_or_create_by(follow_id: 5) do |user|
+  user.followed_id = 6
 end
