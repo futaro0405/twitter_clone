197a2ce803a26dea3562f7f829c7fc7f1c00a015
futaro0405 <fkswtkm0405@gmail.com>
2024-05-03 14:09:45 +0900

    feat: fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,5 +50,8 @@ User.all.each do |user|
     Favorite.find_or_create_by!(user_id: target_user.id) do |favorite|
       favorite.post_id = 0
     end
+    Repost.find_or_create_by!(user_id: target_user.id) do |repost|
+      repost.post_id = 0
+    end
   end
 end
