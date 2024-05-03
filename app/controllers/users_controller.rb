80043b2480bc4f9d374843c8bda4c9832f4b09ad
futaro0405 <fkswtkm0405@gmail.com>
2024-05-03 16:48:27 +0900

    feat: fixed seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :followings, :followers]
+  before_action :authenticate_user!, only: %i[show followings followers]
   before_action :set_user
 
   def show
